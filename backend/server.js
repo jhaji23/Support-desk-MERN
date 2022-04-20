fe8535b8b4143b45bf2096ac1ffb8733f5c8c50a
jhaji23 <ajayhp245@gmail.com>
2022-04-20 11:55:43 +0530

    second  commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
 console.log("welcome to backend");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("hahahaha");
+});
+
+app.listen(5000, () => {
+  console.log(`server started on port ${PORT}`);
+});
